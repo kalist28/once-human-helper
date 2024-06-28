fd5861d26d3c8f1bb564c2dc454d3def02ede53b
Dmitry <kalistratov.d.m@gmail.com>
2024-06-29 01:52:26 +0300

    impl domain lib in app

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -54,6 +54,7 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
+            implementation(project(":domain"))
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
